f0fdbdeeab7ad3a403009c42efd17c9cd18f0c3b
Code With Z <codewithz21@gmail.com>
2022-05-06 15:32:12 +0530

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import UseCaseOneComponent from './components/UseCaseOne/UseCaseOne';
+import UseCaseTwoComponent from './components/UseCaseTwo/UseCaseTwoComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Hinduja React Training</h1>
       <hr></hr>
-      <UseCaseOneComponent />
+      {/* <UseCaseOneComponent /> */}
+      <UseCaseTwoComponent />
     </div>
   );
 }
